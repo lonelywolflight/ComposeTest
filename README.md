a525a32f984a517e81bdf13805c9a88d42c312c1
Max Liu <lonelywolflight1@gmail.com>
2023-07-31 17:57:16 +0800

    Jetpack Compose Row Column and Box Layouts

---
+++ README.md
@@ -0,0 +1,10 @@
+## Jetpack Compose Row, Column & Box (with Examples)
+1. Row
+   1. 相当于LinearLayout的垂直布局
+2. Colum
+   3. 相当于LinearLayout的水平布局
+3. Box
+   4. 相当于FrameLayout
+4. Image
+   1. 使用 ``` painterResource``` 传入图片的id
+   2. contentDescription
--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.example.composetest.ui.theme.ComposeTestTheme
 
@@ -20,7 +29,7 @@ class MainActivity : ComponentActivity() {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    MyUi()
                 }
             }
         }
@@ -28,6 +37,20 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun MyUi(){
+    Image(
+        painter = painterResource(id = R.drawable.lion_g214b21e3b_1280),
+        contentDescription = "Lion and Kid",
+        alignment = Alignment.TopStart
+    )
+
+    Text(
+        text = "Lion and Kid",
+        fontSize = 20.sp,
+        color = Color.Black
+    )
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
