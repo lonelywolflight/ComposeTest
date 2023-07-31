fe105f0e96e3e498318926a0a468fe0f99943967
Max Liu <lonelywolflight1@gmail.com>
2023-07-31 19:11:24 +0800

    Jetpack Compose Alignment and Arrangement (with Examples)

--- README.md
+++ README.md
@@ -8,3 +8,13 @@
 4. Image
    1. 使用 ``` painterResource``` 传入图片的id
    2. contentDescription
+## Jetpack Compose Alignment and Arrangement (with Examples)
+1. Column
+   1. Arrangement.SpaceBetween:
+![img.png](img.png)
+   2. Arrangement.SpaceEvenly:
+![img_1.png](img_1.png)
+   3. Arrangement.SpaceAround:
+![img_2.png](img_2.png)
+2. Row的情况可参考Column
+3. Box,类似FrameLayout
--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -3,23 +3,18 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +23,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     MyUi()
                 }
             }
@@ -38,25 +36,32 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun MyUi(){
-    Image(
-        painter = painterResource(id = R.drawable.lion_g214b21e3b_1280),
-        contentDescription = "Lion and Kid",
-        alignment = Alignment.TopStart
-    )
-
-    Text(
-        text = "Lion and Kid",
-        fontSize = 20.sp,
-        color = Color.Black
-    )
+fun MyUi() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .border(width = 1.dp, color = Color.Magenta),
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(
+            modifier = Modifier.border(width = 1.dp, color = Color.Green),
+            text = "SemicolonSpace 1"
+        )
+        Text(
+            modifier = Modifier.border(width = 1.dp, color = Color.Green),
+            text = "SemicolonSpace 2"
+        )
+        Text(
+            modifier = Modifier.border(width = 1.dp, color = Color.Green),
+            text = "SemicolonSpace 3"
+        )
+    }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
